e9dbde58f28643796519fdcb5e8185b9f67bc88c
laura.mercier <laura.mercier@lauramercierui-MacBookPro.local>
2021-12-17 17:50:50 +0900

    gatsy config edit

--- gatsby-meta-config.js
+++ gatsby-meta-config.js
@@ -15,14 +15,14 @@ module.exports = {
   icon: `content/assets/felog.png`, // Add your favicon
   keywords: [`blog`],
   comment: {
-    //disqusShortName: '', // Your disqus-short-name. check disqus.com.
-    //utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'JaeYeopHan/gatsby-starter-bee', // Your repository for archive comment
   },
   configs: {
     countOfInitialPost: 10, // Config your initial count of post
   },
   sponsor: {
-    //buyMeACoffeeId: 'jbee',
+    buyMeACoffeeId: 'jbee',
   },
   share: {
     facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
